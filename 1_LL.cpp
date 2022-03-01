185c5eb81242f3a18cece8537f554af844baef95
Nidhi Zala <nidhizala2307@gmail.com>
2022-03-01 15:25:39 +0530

    Add files via upload

---
+++ 1_LL.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+struct node
+{
+    int data;
+    struct node* next;
+}*first=NULL;
+
+void create(int a[],int n)
+{
+    int i;
+    struct node *t, *last;
+    first=new node;
+
+    first->data=a[0];
+    first->next=NULL;
+    last=first;
+
+    for(i=1;i<n;i++)
+    {
+        t= new node;
+        t->data=a[i];
+        t->next=NULL;
+
+        last->next=t;
+        last=t; 
+    }
+
+}
+
+void display(struct node *p)
+{
+    while(p!=NULL)
+    {
+        cout<<"data is "<<p->data<<endl;
+        p=p->next;
+    }
+}
+
+int main()
+{
+
+    int a[]={3,4,5,6,6,6};
+    create(a,6);
+    display(first);
+    return 0;
+}
